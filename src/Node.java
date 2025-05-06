25a3d8708bb7438f53d26730bb1a6ca4c422b43a
JSaiyan <183049245+JSaiyan@users.noreply.github.com>
2025-05-05 19:21:36 -0700

    tests pass

--- src/Node.java
+++ src/Node.java
@@ -1,9 +1,11 @@
-public class Node<T> {
+public class Node<T> 
+{
   T value;
   Node<T> left;
   Node<T> right;
 
-  public Node(T value, Node<T> left, Node<T> right) {
+  public Node(T value, Node<T> left, Node<T> right) 
+  {
     this.value = value;
     this.left = left;
     this.right = right;
--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -1,4 +1,5 @@
-public class TraversalPractice {
+public class TraversalPractice
+{
   
   /**
    * Prints the odd values of the nodes in a tree.
@@ -7,9 +8,24 @@ public class TraversalPractice {
    * 
    * @param node The root of the tree to print
    */
-  public static void printOddNodes(Node<Integer> node) {
+  public static void printOddNodes(Node<Integer> node) 
+  {
+    if (node== null) return;
+
+    printOddNodes(node.left); //traverse to left subtree
+
+    printOddNodes(node.right); //travers to right subtree
+
+    if(node.value % 2 != 0) //checks the values of the values are odd
+    {
+      System.out.println(node.value); //print values
+
+    }
+
 
   }
+
+
 
   /**
    * Prints the values of the nodes that have exactly one child in a tree.
@@ -19,9 +35,26 @@ public class TraversalPractice {
    * @param <T> The type of value the nodes hold
    * @param node The root of the tree to print
    */
-  public static <T> void printNodesWithOneChild(Node<T> node) {
+  public static <T> void printNodesWithOneChild(Node<T> node) 
+  {
+    if (node == null) return;
+
+    boolean hasOneChild = (node.left == null && node.right != null) || //checks both nodes if they have a one
+                          (node.left != null && node.right == null);
+
+                if (hasOneChild)
+                {
+                  System.out.println(node.value); //if the operation is true then print the value
+                }
+
+                printNodesWithOneChild(node.left); //travers to left
+                printNodesWithOneChild(node.right); //traverse to right
     
   }
+
+
+
+
 
     /**
    * Returns the sum of the values of all nodes in a tree.
@@ -31,8 +64,12 @@ public class TraversalPractice {
    * @param node The root of the tree
    * @return the sum 
    */
-  public static int treeSum(Node<Integer> node) {
-    return 0;
+  public static int treeSum(Node<Integer> node) 
+  {
+    if (node == null) return 0; //reach the end and node is null return 0
+
+    //adds the values of current node and recurse to left and right and sum them
+    return node.value + treeSum(node.left) + treeSum(node.right); 
   }
 
   /**
@@ -44,8 +81,14 @@ public class TraversalPractice {
    * @param node The root of the tree
    * @return the max value
    */
-  public static int maxVal(Node<Integer> node) {
-    return 0;
+  public static int maxVal(Node<Integer> node) 
+  {
+    if (node == null) return 0;
+
+    int leftVal = maxVal(node.left);
+    int rightVal = maxVal(node.right);
+
+    return Math.max(node.value, Math.max(leftVal, rightVal));
   }
 
   /**
@@ -57,11 +100,21 @@ public class TraversalPractice {
    * @param node The root of the tree
    * @return The number of levels in the tree
    */
-  public static <T> int numLevels(Node<T> node) {
-    return 0;
+  public static <T> int numLevels(Node<T> node) 
+  {
+    if (node == null) return 0;
+
+    int leftL =numLevels(node.left);
+    int rightL = numLevels(node.right);
+
+    return 1 + Math.max(leftL, rightL); //the levels of the tree plus the current node 1
+
   }
 
-  public static void main(String[] args) {
+
+
+  public static void main(String[] args) 
+  {
     /*
      *         99
      *       /    \
@@ -72,7 +125,21 @@ public class TraversalPractice {
 
      // Replace the below line to create a tree 
      // as represented in the diagram above
-     Node<Integer> smallTree = null;
+     //Start from the root three things to pass in, the value 99. 
+     Node<Integer> smallTree = new Node<>(99, new Node<>(45, new Node<>(9, null, null),
+     new Node<>(5, null, null)),
+     new Node<>(82, null, new Node<>(16, null, null))
+     );
+
+
+
+
+
+
+
+
+
+
 
 
     /*
